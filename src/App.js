1889f9e9a3317ae2d1c6ef1383525036de5f4c8e
RonnyKn <ronnykurniawn@gmail.com>
2022-11-22 16:28:00 +0700

    styling header and add responsiveness

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css"
 import React from "react"
 import Header from "./componets/header/Header"
+import Navbar from "./componets/navbar/Navbar"
+import About from "./componets/about/About"
 
 function App() {
   return (
     <>
       <Header />
+      <Navbar />
+      <About />
     </>
   )
 }
--- src/componets/header/Header.js
+++ src/componets/header/Header.js
@@ -1,9 +1,27 @@
 import "./Header.css"
-
 import React from "react"
+import CTA from "./CTA"
+import ME from "../../assets/me.png"
+import HeaderSocials from "./HeaderSocials"
 
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <header>
+      <div className="container header-container">
+        <h5>Hello I'm</h5>
+        <h1>Ronny Kurniawan</h1>
+        <h5 className="text-light">Front-end Developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={ME} alt="" />
+        </div>
+        <div className="scroll-down">
+          <h5>Scroll Down</h5>
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default Header
