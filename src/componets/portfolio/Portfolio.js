a78c3b6cd8abf88062bf44d333326633c09d2cda
RonnyKn <ronnykurniawn@gmail.com>
2022-11-24 15:54:18 +0700

    fix responsiveness

--- src/componets/portfolio/Portfolio.js
+++ src/componets/portfolio/Portfolio.js
@@ -102,6 +102,10 @@ const Portfolio = () => {
             <img src={IMG1} alt="rons_entertaiment" />
           </div>
           <h3>Tittle Portfolio</h3>
+          <h5 className="portfolio-subtitle text-light">
+            YouTube clone made with ReactJs, Material-UI and fetching the data
+            from RapidAPI YouTube v3
+          </h5>
           <div className="portfolio-link">
             <a
               href="https://github.com/"
