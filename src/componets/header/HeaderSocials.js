d382b676d0a45d77992837c4dda7a1758f2e8b3b
ronnykn <ronnykurniawn@gmail.com>
2024-10-04 14:35:36 +0700

    refactor

--- src/componets/header/HeaderSocials.js
+++ src/componets/header/HeaderSocials.js
@@ -1,24 +1,34 @@
-import React from "react"
-import { BsLinkedin, BsGithub, BsInstagram } from "react-icons/bs"
+import React from "react";
+import { BsLinkedin, BsGithub, BsInstagram } from "react-icons/bs";
 
 const HeaderSocials = () => {
+  const socialsData = [
+    {
+      id: 1,
+      icon: <BsGithub />,
+      link: "https://github.com/RonnyKn",
+    },
+    {
+      id: 2,
+      icon: <BsLinkedin />,
+      link: "https://www.linkedin.com/in/ronny-kurniawan-4b205017a/",
+    },
+    {
+      id: 3,
+      icon: <BsInstagram />,
+      link: "https://instagram.com/ronny.kn",
+    },
+  ];
+
   return (
     <div className="header-socials">
-      <a href="https://github.com/RonnyKn" target="_blank" rel="noreferrer">
-        <BsGithub />
-      </a>
-      <a
-        href="https://www.linkedin.com/in/ronny-kurniawan-4b205017a/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <BsLinkedin />
-      </a>
-      <a href="https://instagram.com/ronny.kn" target="_blank" rel="noreferrer">
-        <BsInstagram />
-      </a>
+      {socialsData.map((data) => (
+        <a href={data?.link} target="_blank" rel="noreferrer" key={data?.id}>
+          {data?.icon}
+        </a>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default HeaderSocials
+export default HeaderSocials;
